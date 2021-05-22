4788b316607318c040b3c5b86bef821de7e27486
Asim <asim005011997@gmail.com>
2021-05-22 17:46:23 +0530

    Generated QR code for the input text

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
-
+import QRCode from 'qrcode'
+import React, {useState} from 'react'
+import {Container, Grid, Card, CardContent, makeStyles, TextField, Button} from '@material-ui/core'
 function App() {
+  const classes = useStyle();
+
+  const [text, setText] = useState('')
+  const [imageUrl, setImageUrl] = useState('')
+
+const generateQRCode = async () =>{
+  try {
+    const response = await QRCode.toDataURL(text)
+    setImageUrl(response)
+    console.log(response)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <Container className={classes.useStyle}>
+       <Card>
+       <h2 className={classes.title}>Generate, download and scan QR code with react js</h2>
+         <CardContent>
+           <Grid container spacing={2}>
+             <Grid item xl={4} lg={4} md={6} sm={12} xs={12}>
+               <TextField label="Enter the text"onChange={(e) => setText(e.target.value)} />
+               <Button className={classes.btn} variant="contained" color="primary" onClick={() => generateQRCode()}>Generate</Button>
+                <br/>
+                <br/>
+                <br/>
+                {imageUrl ?(<img src={imageUrl} alt='img'/>) : null}
+             </Grid>
+             <Grid item xl={4} lg={4} md={6} sm={12} xs={12}></Grid>
+             <Grid item xl={4} lg={4} md={6} sm={12} xs={12}></Grid>
+           </Grid>
+         </CardContent>
+       </Card>
+     </Container>
   );
 }
 
+const useStyle = makeStyles((theme) =>({
+  container: {
+    marginTop: 10
+  },
+  title:{
+    display:'flex',
+    justifyItems: 'center',
+    alignItems: 'center',
+    background: '#3f51b5',
+    color: '#fff',
+    padding: 20
+  },
+  btn:{
+    marginTop: 10,
+    marginBottom: 20
+  }
+}))
 export default App;
